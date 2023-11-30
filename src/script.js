a3dc510c3bc5331c99754b1a4cf38507cbaa146f
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2023-11-30 13:03:23 +0100

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,202 @@
+const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+const input = document.querySelector("form > input");
+
+document.addEventListener("DOMContentLoaded", () => {
+	loadDarkModePreference();
+});
+
+const toggleDarkMode = () => {
+  const container = document.querySelector(".container");
+  const isDarkMode = container.classList.toggle("dark-mode");
+  localStorage.setItem("darkMode", isDarkMode ? "on" : "off");
+};
+
+
+const loadDarkModePreference = () => {
+  const darkModeSetting = localStorage.getItem("darkMode");
+  const container = document.querySelector(".container");
+  if (darkModeSetting === "on") {
+    container.classList.add("dark-mode");
+  } else {
+    container.classList.remove("dark-mode");
+  }
+};
+
+
+document.querySelector("#dark-mode-toggle").addEventListener("click", toggleDarkMode);
+
+
+let todos = [];
+
+const showMessage = (message) => {
+	alert(message);
+};
+
+const loadTodos = () => {
+	try {
+		const savedTodos = localStorage.getItem("todos");
+		if (savedTodos) {
+			todos = JSON.parse(savedTodos);
+		}
+	} catch (error) {
+		console.error("Erreur lors du chargement des tâches :", error);
+	}
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	loadTodos();
+	displayTodo();
+});
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const value = input.value;
+	input.value = "";
+	addTodo(value);
+});
+
+document.addEventListener("keydown", (event) => {
+	if (event.key === "Escape" && todos.find((t) => t.editMode)) {
+		todos.find((t) => t.editMode).editMode = false;
+		displayTodo();
+	}
+});
+
+const displayTodo = () => {
+	const todosNode = todos.map((todo, index) => {
+		if (todo.editMode) {
+			return createTodoEditElement(todo, index);
+		} else {
+			return createTodoElement(todo, index);
+		}
+	});
+	ul.innerHTML = "";
+	ul.append(...todosNode);
+};
+
+const createTodoElement = (todo, index) => {
+	const li = document.createElement("li");
+
+	const buttonDelete = document.createElement("button");
+	buttonDelete.innerHTML = "Supprimer";
+	buttonDelete.classList.add("danger");
+	buttonDelete.setAttribute("aria-label", "Supprimer la tâche");
+	buttonDelete.addEventListener("click", (event) => {
+		event.stopPropagation();
+		deleteTodo(index);
+	});
+
+	const buttonEdit = document.createElement("button");
+	buttonEdit.innerHTML = "Edit";
+	buttonEdit.classList.add("primary");
+	buttonEdit.setAttribute("aria-label", "Modifier la tâche");
+	buttonEdit.addEventListener("click", (event) => {
+		event.stopPropagation();
+		toggleEditMode(index);
+	});
+
+	li.innerHTML = `
+    <span class="todo ${todo.done ? "done" : ""}" role="checkbox" aria-checked="${todo.done}"></span>
+    <p class="${todo.done ? "done" : ""}">${todo.text}</p>
+  `;
+
+	let timer;
+	li.addEventListener("click", (event) => {
+		if (event.detail === 1) {
+			timer = setTimeout(() => {
+				toggleTodo(index);
+			}, 200);
+		} else if (event.detail > 1) {
+			clearTimeout(timer);
+			toggleEditMode(index);
+		}
+	});
+
+	li.append(buttonEdit, buttonDelete);
+	return li;
+};
+
+const createTodoEditElement = (todo, index) => {
+	const li = document.createElement("li");
+	const input = document.createElement("input");
+	input.type = "text";
+	input.value = todo.text;
+	input.addEventListener("keydown", (event) => {
+		if (event.key === "Enter") {
+			editTodo(index, input);
+		}
+	});
+	const buttonSave = document.createElement("button");
+	buttonSave.innerHTML = "Save";
+	buttonSave.classList.add("success");
+	const buttonCancel = document.createElement("button");
+	buttonCancel.innerHTML = "Cancel";
+	buttonCancel.classList.add("danger");
+	buttonCancel.addEventListener("click", (event) => {
+		event.stopPropagation();
+		toggleEditMode(index);
+	});
+  buttonSave.addEventListener('click', (event) => {
+    editTodo(index, input.value); 
+  });
+	li.append(input, buttonSave, buttonCancel);
+	setTimeout(() => input.focus(), 0);
+	return li;
+};
+
+const addTodo = (text) => {
+	text = text.trim();
+	if (!text) {
+		alert("La tâche ne peut pas être vide.");
+		return; 
+	}
+	todos.push({
+		text: `${text[0].toUpperCase()}${text.slice(1)}`,
+		done: false,
+	});
+	displayTodo();
+	saveTodos();
+	showMessage("Tâche ajoutée avec succès.");
+};
+
+const saveTodos = () => {
+	try {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	} catch (error) {
+		console.error("Erreur lors de l'enregistrement des tâches :", error);
+	}
+};
+
+const deleteTodo = (index) => {
+	todos.splice(index, 1);
+	displayTodo();
+	saveTodos();
+	showMessage("Tâche supprimée.");
+};
+
+const toggleTodo = (index) => {
+	todos[index].done = !todos[index].done;
+	displayTodo();
+	saveTodos();
+};
+
+const toggleEditMode = (index) => {
+	todos[index].editMode = !todos[index].editMode;
+	displayTodo();
+};
+
+const editTodo = (index, newTextInput) => {
+	const value = newTextInput.value.trim();
+	if (!value) {
+		alert("La tâche ne peut pas être vide.");
+		return;
+	}
+  todos[index].text = newTextInput;
+  todos[index].editMode = false;
+	displayTodo();
+	saveTodos();
+	showMessage("Tâche mise à jour.");
+};
+
+displayTodo();
